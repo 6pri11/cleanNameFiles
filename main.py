f8b5d26f93bc56054ac2f103b7a2699ac6083c69
Cyprien <91326577+6pri11@users.noreply.github.com>
2025-02-26 22:09:35 +0100

    counter if name exist

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import os
 import sys
 from unidecode import unidecode
 
+# ARGUMENT 1 : Chemin du dossier à traiter
 path = sys.argv[1]
+nbr_files = len(os.listdir(path))
+nbr_files_treated = 0
+files_treated = set(os.listdir(path))
 
 def clean_name_files(path):
+    global nbr_files_treated 
     for file in os.listdir(path):
         new_name = clean_filename(file)
         if new_name:
             old_path = os.path.join(path, file)
             new_path = os.path.join(path, new_name)
             if old_path != new_path: 
-                print(f"Renaming {old_path} to {new_path}")
                 os.rename(old_path, new_path)
+                files_treated.add(new_name)
+                nbr_files_treated += 1
+                print(f"{nbr_files_treated} fichiers traités sur {nbr_files}")          
 
 def clean_filename(name):
     base = os.path.splitext(name)[0]
@@ -28,7 +35,11 @@ def clean_filename(name):
     base = base.replace('^', '_').replace('~', '_')
     base = base.lower()
     
+    counter = 1
     new_name = base + ext
+    while new_name in files_treated:
+        new_name = base + f"_{counter}" + ext
+        counter += 1
     
     print(new_name)
     return new_name
